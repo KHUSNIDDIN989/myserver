f458a88521680fdcc63ae849b8796d9bc2993d3d
Husniddin <khusniddin989@gmail.com>
2023-05-26 15:37:37 +0500

    fixed get

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ app.use(cors("*"));
 app.use(express.json());
 app.use("/", router);
 
-app.get('/get', (req, res) => { res.json("Hech narsa topilmadi!")})
+app.get("/get", (req, res) => res.json("Hech narsa topilmadi!"));
 
 app.use("/*", (_, res) => res.json("Not Found 404").status(404));
 app.listen(9000, () => console.log(9000));
